5b5230076efbc4aad7c3b0ca05541d4ce19f05df
Sovianum <aklyukvin@yandex.ru>
2016-09-28 13:52:37 +0300

    Passed task 6.

--- module_1/Task_6/main.cpp
+++ module_1/Task_6/main.cpp
@@ -6,25 +6,40 @@
 #include <iostream>
 #include <assert.h>
 
-void inner_get_var_num(int cubes_left, int last_row, int& var_num) {
+void inner_get_var_num(int cubes_left, int last_row, long& var_num, long* result_table, int table_length) {
     assert(cubes_left >= 0);
+    int cell_coord = cubes_left * table_length + last_row;
+    if (result_table[cell_coord] >= 0) {
+        var_num += result_table[cell_coord];
+        return;
+    }
+
+    long local_var_num = var_num;    // переменная нужна для того, чтобы внести запись в result_table
+
     if (cubes_left == 0) {
         ++var_num;
         return;
     }
 
-    for (int new_row = 1; new_row < last_row && new_row <= cubes_left; ++new_row) {    // решается задача с условием <=
-        inner_get_var_num(cubes_left - new_row, new_row, var_num);
+    for (int new_row = 1; new_row < last_row && new_row <= cubes_left; ++new_row) {
+        inner_get_var_num(cubes_left - new_row, new_row, var_num, result_table, table_length);
     }
+
+    result_table[cell_coord] = var_num - local_var_num;
 }
 
-int get_var_num(int cubes_num) {
-    int var_num = 0;
-
-    for (int first_row = 1; first_row <= cubes_num; ++first_row) {
-        inner_get_var_num(cubes_num - first_row, first_row, var_num);
+long int get_var_num(int cubes_num) {
+    long var_num = 0;
+    long* result_table = new long[(cubes_num + 1) * (cubes_num + 1)];
+    for (int i = 0; i <= cubes_num * cubes_num; ++i) {
+        result_table[i] = -1;
     }
 
+    for (int first_row = 1; first_row <= cubes_num; ++first_row) {
+        inner_get_var_num(cubes_num - first_row, first_row, var_num, result_table, cubes_num);
+    }
+
+    delete[] result_table;
     return var_num;
 }
 
--- module_1/Task_7/main.cpp
+++ module_1/Task_7/main.cpp
@@ -1,4 +1,16 @@
+/*
+ * В город N приехал цирк с командой атлетов. Они хотя удивить горожан города N - выстроить из своих тел башню максимальной высоты.
+ * Башня - это цепочка атлетов, первый стоит на земле, второй стоит у него на плечах, третий стоит на плечах у второго и т.д.
+ * Каждый атлет характеризуется силой s_i (kg) и массой m_i (kg). Сила - это максимальная масса, которую атлет способен держать у себя на плечах.
+ * К сожалению, ни один из атлетов не умеет программировать, так как всю жизнь они занимались физической подготовкой, и у них
+ * не было времени на изучение языков программирования. Помогите им, напишите программу, которая определит максимальную высоту башни, которую они
+ * могут составить.
+ * Известно, что если атлет тяжелее, то он и сильнее: если m_i > m_j, то s_i > s_j.
+ * Атлеты равной массы могут иметь различную силу.
+ */
+
 #include <iostream>
+#include <assert.h>
 
 
 class Athlete {
@@ -9,42 +21,177 @@ public:
     int strength;
 };
 
-
 class Node {
 public:
+    friend class List;
+
     Node() {
+        this->prev = NULL;
         this->next = NULL;
         this->state = NULL;
     }
 
-    Node(Athlete& athlete) {
-        this->next = NULL;
-        this->state = new Athlete(athlete.mass, athlete.strength);
-    }
-
-    void set_next(Node& node) {
-        *(this->next) = node;
-    }
-
-    void insert_after(Athlete element) {
-        Node node = Node(element);
-
-        if (this->next == NULL) {
-            set_next(element);
+    ~Node() {
+        delete this->state;
+        if (this->next != NULL) {
+            delete(this->next);
         }
+    }
 
-        if ()
+    Node(Athlete& element) {
+        this->next = NULL;
+        this->state = new Athlete(element.mass, element.strength);
+    }
 
+    Athlete& get_state() {
+        return *state;
+    }
+
+    Node* get_next() {
+        return this->next;
+    }
+
+    Node* get_prev() {
+        return this->prev;
+    }
+
+    bool is_head() {
+        return this->prev == NULL;
+    }
+
+    bool is_tail() {
+        return this->next == NULL;
     }
 
 
 private:
     Athlete* state;
     Node* next;
+    Node* prev;
 };
 
 
+class List {
+public:
+    List() {
+        this->head = NULL;
+        this->tail = this->head;
+    }
+
+    ~List() {
+        delete this->head;
+    }
+
+    bool is_empty() {
+        return this->head == NULL;
+    }
+
+    Node* get_head() {
+        return this->head;
+    }
+
+    void insert_after(Node& node, Athlete& state) {
+        Node* new_node = new Node(state);
+        new_node->next = node.next;
+        new_node->prev = &node;
+
+        if (node.next->prev != NULL) {
+            node.next->prev = new_node;
+        }
+        node.next = new_node;
+    }
+
+    void push_back(Athlete& state) {
+        if (this->is_empty()) {
+            this->head = new Node(state);
+            this->tail = this->head;
+            return;
+        }
+
+        Node* new_node_ptr = new Node(state);
+        this->tail->next = new_node_ptr;
+        new_node_ptr->prev = this->tail;
+        this->tail = new_node_ptr;
+
+
+    }
+
+    void remove_element(Node* node) {
+        if (node->next != NULL) {
+            node->next->prev = node->prev;
+        }
+
+        if (node->prev != NULL) {
+            node->prev->next = node->next;
+        }
+
+        node->next = node->prev = NULL;
+        delete node;
+    }
+
+private:
+    Node* head;
+    Node* tail;
+};
+
+
+Node* get_strongest_athlete_node(List& list) {
+    assert(!list.is_empty());
+    Node* node_ptr = list.get_head();
+    int max_strength = -1;
+    Node* strongest = NULL;
+
+    while (!node_ptr->is_tail()) {
+        if (node_ptr->get_state().strength > max_strength) {
+            max_strength = node_ptr->get_state().strength;
+            strongest = node_ptr;
+        }
+
+        node_ptr = node_ptr->get_next();
+    }
+
+    return strongest;
+}
+
+Node* get_next_athlete_node(List& list, int& strength_left) {
+    assert(!list.is_empty());
+
+    Node* node_ptr = list.get_head();
+    Node* next_athlete_node = NULL;
+
+    int new_strength_left = -1;
+
+    while (!node_ptr->is_tail()) {
+        Athlete athlete = node_ptr->get_state();
+        if (athlete.mass <= strength_left) {      // если предыдущие атлеты выдержат нового
+            int old_athlete_strength_left = strength_left - athlete.mass;   // предел сил старых атлетов с учетом добавления нового
+
+            if (athlete.strength > new_strength_left && old_athlete_strength_left > new_strength_left) { // если добавление этого атлета приводит к более слабому ослаблению башни
+                new_strength_left = old_athlete_strength_left > athlete.strength ? athlete.strength : old_athlete_strength_left;  // выбираем слабое звено
+                next_athlete_node = node_ptr;
+            }
+        }
+
+        node_ptr = node_ptr->get_next();
+    }
+
+    return next_athlete_node;
+
+}
+
+void test_list() {
+    List list = List();
+    Athlete athlete = Athlete(10, 10);
+    list.push_back(athlete);
+    Node* head = list.get_head();
+    std::cout << head->get_state().mass << ' ' << head->get_state().strength;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int i = 0;
+    while (std::cin >> i) {
+        std::cout << i << std::endl;
+    }
+    std::cout << "Done";
     return 0;
 }
